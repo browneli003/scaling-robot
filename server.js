b781d61df76acd5977de28d53219eafd2e75649c
Elia Brown <brownelijah143@gmail.com>
2021-11-15 23:14:42 -0700

    yseyysyyy

--- server.js
+++ server.js
@@ -4,13 +4,14 @@ const routes = require('./controllers');
 const path = require('path');
 const helpers = require('./utils/helpers');
 const session = require('express-session');
+const exphbs = require('express-handlebars');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sess = {
   // change secret and use .env
   secret: 'Super secret secret',
   cookie: {},
   resave: false,
-  saveUninitialized: true,
+  saveUninitialized: false,
   store: new SequelizeStore({
     db: sequelize
   })
@@ -19,7 +20,7 @@ const sess = {
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const exphbs = require('express-handlebars');
+
 const hbs = exphbs.create({ helpers });
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
